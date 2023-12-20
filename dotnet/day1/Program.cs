5481e2cf9b22ecf0312d6a91840473486e5d353d
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2023-12-20 12:32:04 +0000

    day1

---
+++ dotnet/day1/Program.cs
@@ -0,0 +1,35 @@
+﻿var lines = File.ReadLines(Directory.GetCurrentDirectory() + "/input.txt");
+
+int sum = 0;
+var numberNames = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+foreach (var line in lines)
+{
+    var digits = new List<int>();
+    for (int i = 0; i < line.Length; i++)
+    {
+        char character = line[i];
+        if (int.TryParse(character.ToString(), out var tmp))
+        {
+            digits.Add(tmp);
+        }
+        else
+        {
+            var n = numberNames.FirstOrDefault(x => x.StartsWith(character) && line.Substring(i, x.Length + i <= line.Length ? x.Length : 0) == x);
+            if (n != null)
+            {
+                digits.Add(numberNames.IndexOf(n) + 1);
+                i += n.Length - 2;
+            }
+        }
+
+    }
+
+    var digit = digits.First() + "" + digits.Last();
+    Console.WriteLine(digit);
+    sum += int.Parse(digit);
+}
+
+
+Console.WriteLine(sum);
+
